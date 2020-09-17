fd064a9ef173fbe11d1417fc01c3311a9d91d893
cauld19 <50849710+cauld19@users.noreply.github.com>
2020-09-16 19:23:29 -0700

    add vertex / edge

--- projects/graph/graph.py
+++ projects/graph/graph.py
@@ -8,17 +8,17 @@ class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
     def __init__(self):
         self.vertices = {}
+        
 
     def add_vertex(self, vertex_id):
         """
         Add a vertex to the graph.
         """
-        pass  # TODO
+        self.vertices[vertex_id] = set()
+        
 
     def add_edge(self, v1, v2):
-        """
-        Add a directed edge to the graph.
-        """
+        self.vertices[v1].add(v2)
         pass  # TODO
 
     def get_neighbors(self, vertex_id):
